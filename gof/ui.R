a2e57531ee559f549da268038df88c887d1c9e3d
Tien Phan <31216432+tpou@users.noreply.github.com>
2023-05-12 14:31:18 +0700

    Update ui.R

--- gof/ui.R
+++ gof/ui.R
@@ -18,46 +18,19 @@ fluidPage(
     title = 'Goodness of Fit',
     
     h1('Test'),
+  titlePanel("CSV Upload and Table/Plot Display"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Choose CSV File", accept = c("text/csv","text/comma-separated-values,text/plain",".csv")
+    ),
+      checkboxInput("header","Header", TRUE),),
     
-    sidebarLayout(
-      
-      sidebarPanel(
-        fileInput("file", "Choose CSV File",
-                  accept = c(
-                    "text/csv",
-                    "text/comma-separated-values,text/plain",
-                    ".csv")
-        ), #end_fileInput
-        
-        DTOutput("mytable"),
-        
-        tags$hr(),
-        
-        checkboxInput("header", "Header", TRUE),
-        actionButton("myButton","Plot"),
-        actionButton('myButtongo', 'Update')
-      ), #end_sidebarPanel
-      
-      mainPanel(
-        tabsetPanel(
-          tabPanel("Table",DT::dataTableOutput("head")),
-          tabPanel("Plot",plotOutput('myplot')),
-          tabPanel("Plot Update", plotOutput('myplot2'))
-        )
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Table", DT::dataTableOutput("table")),
+        tabPanel("Plot", plotOutput("plot"))
       )
-      #mainPanel(
-      #  fluidRow(
-      #          column(6, dt_output('Test','contents')),
-      #          column(6, plotOutput('x1', height = 500))
-      #  )
-      ), #end_mainPanel
-    # Define JavaScript code to handle cell clicks ----
-    tags$script('
-    $(document).on("click", "#DataTables_Table_1 td", function() {
-      var col_idx = $(this).index();
-      var row_idx = $(this).parent().index();
-      Shiny.setInputValue("cell_edit", [row_idx, col_idx]);
-    });
-  ')
+    )
+  )
 
 )
