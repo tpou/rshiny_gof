9a5965387cd64ecf4fbec97989b8b413727da9a6
Tpou <tienphn@gmail.com>
2023-05-13 21:48:40 +0700

    gof_update

--- gof/server.R
+++ gof/server.R
@@ -11,6 +11,22 @@ library(shiny)
 library(ggplot2)
 library(DT)
 library(tibble)
+library(fitdistrplus)
+library(EnvStats)
+library(plotly)
+library(viridis)
+# define goodness of fitness function
+gof_fit <- function(data, dist_type, test_type){ 
+  
+  # dist_type can be 'norm','lnorm','pois','exp','gamma','bninom','geom','beta','unif','logis'
+  # test_type: sw (Shapiro-Wilk), ad, cmv, lillie, skew, chisq, ks
+  
+  fit_func <- fitdist(data, dist_type)
+  gof = gofTest(data,distribution = dist_type, test = test_type)
+  
+  plot(fit_func)
+  print(gof)
+}
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
@@ -22,9 +38,12 @@ function(input, output, session) {
   observeEvent(input$file, {
     req(input$file)
     values$data <- read.csv(input$file$datapath, header=input$header, sep=",")
-    updateSelectInput(session, "column1", choices=names(values$data))
-    updateSelectInput(session, "column2", choices=names(values$data))
-  })
+    updateSelectInput(session, "column1_tabcdf", choices=names(values$data))
+    updateSelectInput(session, "column1_tabplot", choices=names(values$data))
+    updateSelectInput(session, "column2_tabplot", choices=names(values$data))
+    updateSelectInput(session, "column3_tabplot", choices=names(values$data))
+    updateSelectInput(session, "column4_tabplot", choices=names(values$data))
+    })
   
   # Display the contents of the uploaded CSV file in an editable DT table
   output$table <- DT::renderDataTable({
@@ -34,13 +53,50 @@ function(input, output, session) {
   })
   
   # Create a plot of the uploaded CSV data using ggplot2 and dynamically update the plot
-  output$plot <- renderPlot({
+  output$plot <- renderPlotly({
     req(values$data)
-      x_col <- grep(input$column1,colnames(values$data))
-      y_col <- grep(input$column2,colnames(values$data))
-    ggplot(values$data, aes(x=values$data[[x_col]], values$data[[y_col]])) + 
-      geom_point() +
-      labs(x = "X", y = "Y", title = "Uploaded CSV Data Plot")
+      x_col <- grep(input$column1_tabplot,colnames(values$data))
+      y_col <- grep(input$column2_tabplot,colnames(values$data))
+      size <- grep(input$column3_tabplot,colnames(values$data))
+      fill <- grep(input$column4_tabplot,colnames(values$data))
+    g1<-ggplot(values$data, aes(x=values$data[[x_col]],y=values$data[[y_col]],
+                                size=values$data[[size]],
+                                fill=values$data[[fill]],
+                                text=paste(input$column1_tabplot,":",values$data[[x_col]],br(),input$column2_tabplot,":",values$data[[y_col]]))) + 
+      geom_point(shape=21,color="black",fill="pink", stroke=0.5, alpha=0.8) +
+      scale_fill_viridis(discrete=TRUE, guide=FALSE, option="B") +
+      labs(x = input$column1_tabplot, y = input$column2_tabplot, title = "Data Plot")
+    ggplotly(g1, tooltip="text")
+  })
+  
+  # Create CDF composite plot
+  output$plot1 <- renderPlot({
+    req(values$data)
+    fit_data <- grep(input$column1_tabcdf, colnames(values$data))
+    fit_norm <- fitdist(values$data[[fit_data]], "norm")
+    fit_logn <- fitdist(values$data[[fit_data]], "lnorm")
+    #fit_gama <- fitdist(values$data[[fit_data]], "gamma")
+    #fit_exp <- fitdist(values$data[[fit_data]], "exp")
+    cdfcomp(list(fit_norm, fit_logn),fitlty=c(2,6),fitcol=c("blue","green"),fitlwd=c(4,4),legendtext = c("Normal","LogNormal"))
+  })
+  
+  # Create the plot2-GoF when the data is selected
+  output$plot2 <- renderPlot({
+    req(values$data)
+    fit_data <- grep(input$column1_tabcdf,colnames(values$data))
+    fit_dist <- fitdist(values$data[[fit_data]],input$column1_tabgof)
+    
+    plot(fit_dist)
+  })
+  
+  # render Print data of GOF
+  output$fitdist <- renderPrint({
+    req(values$data)
+    fit_data <- grep(input$column1_tabcdf,colnames(values$data))
+    gof = gofTest(values$data[[fit_data]],distribution = input$column1_tabgof, test = "chisq")
+    print(gof)
+    #tagList('Test',"B", gof_prt)
+    #print(gof);
   })
   
   # Update the uploaded data when the user makes changes to the table
@@ -50,18 +106,11 @@ function(input, output, session) {
     col_idx <- info$col + 1
     value <- info$value
     
-    # Update the data object using reactiveValues
-    values$data[row_idx, col_idx] <- value
+  # Update the data object using reactiveValues
+  values$data[row_idx, col_idx] <- value
 
-    # Update the plot when the table is edited
-    output$plot <- renderPlot({
-      req(values$data)
-      x_col <- grep(input$column1,colnames(values$data))
-      y_col <- grep(input$column2,colnames(values$data))
-      ggplot(values$data, aes(x=values$data[[x_col]], values$data[[y_col]])) + 
-        geom_point() +
-        labs(x = "X", y = "Y", title = "Uploaded CSV Data Plot")
-    })
+  
+    
   })
   
 }
--- gof/ui.R
+++ gof/ui.R
@@ -1,38 +1,108 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
 library(DT)
 library(tidyverse)
+library(shinydashboard)
+library(plotly)
 
-# Define UI for application that draws a histogram
-fluidPage(
+# Define UI 
+ui <- dashboardPage(skin="black",
+                    dashboardHeader(titleWidth=200),
+                    dashboardSidebar(width = 250,
+                                     sidebarMenu(
+                                       menuItem("Statistical Test", tabName= "stattest")
+                                     ) # end sidebarMenu
+                                     ), # end dashboardSidebar
+                    dashboardBody(
+                      tabItems(
+                        tabItem(tabName ="stattest",
+                                tabBox(
+                                       id = "tabbox1",
+                                       width = 12,
+                                       tabPanel("Data Load",
+                                                sidebarLayout(
+                                                  sidebarPanel(
+                                                    fileInput("file", "Choose CSV File", accept = ".csv")
+                                                  ),
+                                                  mainPanel(
+                                                  checkboxInput("header","Header", TRUE)))
+                                                ),
+                                       tabPanel("Data Description",
+                                                ),
+                                       tabPanel("Data Table",
+                                                h4("This Table is editable."),
+                                                
+                                                h5("How: Double-click on individual data cell to make change."),
+                                                h6("Plots and analysis in the subsequent tabs are updated interactively !!!"),
+                                                tags$style("h6{color: red; font-style:italic;}","h5{color:blue}"),
+                                
+                                                DT::dataTableOutput("table")),
+                                       tabPanel("Data Plot",
+                                                h5("X and Y are selected from dropdown menus below."),
+                                                fluidRow(
+                                                column(width = 3,selectInput("column1_tabplot", "Select X axis", NULL)),
+                                                column(width = 3,selectInput("column2_tabplot", "Select Y axis", NULL)),
+                                                column(width = 3,selectInput("column3_tabplot", "Size", NULL)), 
+                                                column(width = 3,selectInput("column4_tabplot", "Fill", NULL))),# end fluidRow
+                                                plotlyOutput("plot", height=600)),
+                                       tabPanel("CDF Comp",
+                                                fluidRow(
+                                                column(width = 3,selectInput("column1_tabcdf", "Univariate Parameter", NULL))  
+                                                ), # end fluidRow
+                                                h5("Plot shows different Cumulative Density Function (CDF) curves."),
+                                                h5("The empirical CDF showed in black dotted-line indicating the actual data, in comparision with other theoretical CDFs which computed from normal and lognormal parameters.",style="color:black;"),
+                                                h5("The plot can be used to quickly interpret which distribution type could be matched the data.",style="color:green;"),
+                                                plotOutput("plot1",width=800, height=600)),
+                                       tabPanel("Goodness of Fitness",
+                                                h4("Goodness of Fit Test", style="color:green;"),
+                                                h5("Data is selected from the tab CDF Comp > Univariate Paramter"),
+                           
+                                                fluidRow(
+                                                  column(width = 3,selectInput("column1_tabgof","Distribution",c("Normal"="norm","LogNormal"="lnorm","Gamma"="gamma","Exponential"="exp"))),
+                                                  column(width = 3,selectInput("column2_tabgof", "Statistical Test Method",c("Chi-square"="chisq")))
+                                              
+                                                  ),
+                                                h4("Visual Check:",style="color:red;"),
+                                                h5("From left-right and top-bottom are histogram, quantile-quantile (Q-Q), cumulative density function, and probability-probability plots."),
+                                                plotOutput("plot2", height=800),
+                                                h4("Statistical Test:",style="color:red;"),
+                                                h5("Goodness-of-Fit Test is interpreted using Null Hypothesis."),
+                                                h5("p-value is computed from statistical test method and to compare with significance level [alpha], hereby is set to 0.05 to make inferrence on hythothesis.",style="font-style:italic;"),
+                                                h5("If p-value > 0.05, it can be inferred that the sample data is generated from a selected theoretical distribution.",style="color:green;"),
+                                                verbatimTextOutput("fitdist")),
+                               
+ 
+                                       ) # end tabBox
+                                )
+                      )
+                    )
+                    ) # end dashboardPage
 
-    title = 'Goodness of Fit',
-    
-    h1('Test'),
-  titlePanel("CSV Upload and Table/Plot Display"),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("file", "Choose CSV File", accept = c("text/csv","text/comma-separated-values,text/plain",".csv")
-    ),
-      checkboxInput("header","Header", TRUE),),
-      selectInput("column1", "Select X Column", NULL),
-      selectInput("column2", "Select Y Column", NULL)
-    
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Table", DT::dataTableOutput("table")),
-        tabPanel("Plot", plotOutput("plot"))
-      )
-    )
-  )
+# fluidPage(
+#   title = 'Goodness of Fit Statistical Test',
+#   h1('Goodness of Fitness'),
+#   titlePanel("Upload CSV file and Table/Plot Display"),
+#   
+#   sidebarLayout(
+#     sidebarPanel(
+#       fileInput("file", "Choose CSV File", accept = ".csv")
+#     ),
+#       checkboxInput("header","Header", TRUE)),
+#       selectInput("column1", "Select X Column", NULL),
+#       selectInput("column2", "Select Y Column", NULL),
+#       
+#       selectInput("column3", "Select data for GoF", NULL),
+#       selectInput("column4", "Distribution:",c("Normal"="norm","LogNormal"="lnorm","Gamma"="gamma","Exponential"="exp")),
+#       selectInput("column5", "Statistical Test Method:",c("Chi-squared"="chisq")),
+#     mainPanel(
+#       tabsetPanel(
+#         tabPanel("Data Table", DT::dataTableOutput("table")),
+#         tabPanel("Data Plot", plotOutput("plot")),
+#         tabPanel("CDF Comp", plotOutput("plot1")),
+#         tabPanel("Goodness of Fitness", plotOutput("plot2"), verbatimTextOutput("fitdist")),
+#         tabPanel("Markdown")
+#         )
+#     )
+#   )
 
-)
