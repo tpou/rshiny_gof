214b24b780b8f17046480dd3311f80d36074432a
Tien Phan <31216432+tpou@users.noreply.github.com>
2023-05-25 15:27:53 +0700

    Update ui.R

--- gof/ui.R
+++ gof/ui.R
@@ -9,63 +9,123 @@ library(plotly)
 # Define UI 
 ui <- dashboardPage(skin="black",
                     dashboardHeader(titleWidth=200),
-                    dashboardSidebar(width = 250,
+                    dashboardSidebar(width = 200,
                                      sidebarMenu(
-                                       menuItem("Statistical Test", tabName= "stattest")
+                                       menuItem("Data Load", tabName= "dataload"),
+                                       menuItem("Data View", tabName ="dataview"),
+                                       menuItem("Data Viz", tabName = "dataviz"),
+                                       menuItem("Statiscal Test", tabName ="statisticaltest")
                                      ) # end sidebarMenu
                                      ), # end dashboardSidebar
                     dashboardBody(
                       tabItems(
-                        tabItem(tabName ="stattest",
+                        tabItem(tabName="dataload",
+                                fluidRow(
+                                  tabBox(
+                                    id = "tabbox1",
+                                    width = 12,
+    
+                                    tabPanel("Data Load",
+                                             sidebarLayout(
+                                               sidebarPanel(
+                                                 fileInput("file", "Choose Excel/CSV File", accept = c(".csv",".xlsx",".xls"))
+                                               ),
+                                               mainPanel(
+                                                 checkboxInput("header","Header", TRUE),
+                                                 selectInput("filetype", "File Type", choices=list("EXCEL", "CSV"))))
+                                    )
+                            
+                                  ) # end tabBox
+                                )#endfluidRow
+                                ),
+                        
+                        tabItem(tabName="dataview",
+                                fluidRow(
+                                  tabBox(
+                                    id = "tabbox2",
+                                    width = 12,
+      
+                                    tabPanel("Data View",
+                                             h4("This Table is editable."),
+                                             h5("How: Double-click on individual data cell to make change."),
+                                             h6("Plots and analysis in the subsequent tabs are updated interactively !!!"),
+                                             fluidRow(
+                                               column(width = 4, selectInput('first_filter', "Filter by Column 1:", NULL)),
+                                               column(width = 4, selectInput('first_filter_value', 'Value 1:',NULL))
+                                             ),
+                                             fluidRow(
+                                               column(width = 4, selectInput('second_filter', "Filter by Column 2:", NULL)),
+                                               column(width = 4, selectInput('second_filter_value', 'Value 2:',NULL))
+                                             ),
+                                             tags$style("h6{color: red; font-style:italic;}","h5{color:blue}"),
+                                             
+                                             DT::dataTableOutput("table")),
+                                    tabPanel("Data Description",
+                                             h4("Data Summary"),
+                                             verbatimTextOutput("datadescript"),
+                                             h4("Data Report"),
+                                             fluidRow(
+                                               column(width = 4, actionButton("report_maid","Generate Data Maid Report", class="btn-success")),
+                                               column(width = 4, actionButton("report_explorer","Generate Data Explorer", class="btn-primary", style="color:white;")),
+                                               column(width = 4, actionButton("report_smart","Generate Data Smart", class="btn-warning", style="color:white;"))
+                                             ),
+                                             #actionButton("report_maid","Generate Data Maid Report", class="btn-success"),
+                                             #actionButton("report_explorer","Generate Data Explorer", class="btn-primary", style="color:white;margin-right:5px;"),
+                                             #actionButton("report_smart","Generate Data Smart", class="btn-warning", style="color:white;margin-right:5px;"),
+                                             verbatimTextOutput("error"),
+                                             htmlOutput("report")
+                                             
+                                    )
+                                    ) # end tabBox
+                                ) #endfluidRow
+                                ),
+                        tabItem(tabName="dataviz",
+                                fluidRow(
+                                  tabBox(
+                                    id = "tabbox3",
+                                    width = 12,
+                                   
+                                      tabPanel("Univariate",
+                                             fluidRow(
+                                               h2("Univariate"),
+                                               selectInput("column1_univ", "Select Variable:", NULL),
+                                               box(
+                                                 title ='Histogram', status='primary', solidHeader = TRUE,collapsible = TRUE,
+                                                 sliderInput("number_of_bins", "Choose the bin size:",value=10,min=0,max=100,step=2),
+                                                 plotlyOutput("plotviz1",height=250)
+                                               ),
+                                               box(
+                                                 title ='Density v2', status='warning', solidHeader = TRUE,collapsible = TRUE,
+                                                 selectInput("column2_univ", "Filtered By", NULL),
+                                                 plotlyOutput("plotviz1.2",height=250)
+                                               )
+                                               
+                                             ) # endFluidRow
+                                             
+                                        ),
+                                    tabPanel("Bivariates",
+                                             fluidRow(
+                                               box(
+                                                 title ='Scatter Plot', status='info', solidHeader = TRUE, collapsible = TRUE,
+                                                 h5("X and Y are selected from dropdown menus below."),
+                                                 fluidRow(
+                                                   column(width = 3,selectInput("column1_tabplot", "Select X axis", NULL)),
+                                                   column(width = 3,selectInput("column2_tabplot", "Select Y axis", NULL)),
+                                                   column(width = 3,selectInput("column3_tabplot", "Size", NULL))),# end fluidRow
+                                                 plotlyOutput("plotviz2", height=250)
+                                               )
+                                             ) #endfluidRow
+                                             )
+                                    
+                                  ) # end tabBox
+                                ) #endFluidRow
+                                ),
+                        tabItem(tabName ="statisticaltest",
+                                fluidRow(
                                 tabBox(
-                                       id = "tabbox1",
+                                       id = "tabbox4",
                                        width = 12,
-                                       tabPanel("Data Load",
-                                                sidebarLayout(
-                                                  sidebarPanel(
-                                                    fileInput("file", "Choose Excel/CSV File", accept = c(".csv",".xlsx",".xls"))
-                                                  ),
-                                                  mainPanel(
-                                                  checkboxInput("header","Header", TRUE),
-                                                  selectInput("filetype", "File Type", choices=list("EXCEL", "CSV"))))
-                                                ),
-                                       tabPanel("Data Table",
-                                                h4("This Table is editable."),
-                                                h5("How: Double-click on individual data cell to make change."),
-                                                h6("Plots and analysis in the subsequent tabs are updated interactively !!!"),
-                                                fluidRow(
-                                                  column(width = 4, selectInput('first_filter', "Filter by Column:", NULL)),
-                                                  column(width = 4, selectInput('first_filter_value', 'Value:',NULL))
-                                                ),
-                                                tags$style("h6{color: red; font-style:italic;}","h5{color:blue}"),
-                                                
-                                                DT::dataTableOutput("table")),
-                                       tabPanel("Data Description",
-                                                h4("Data Summary"),
-                                                verbatimTextOutput("datadescript"),
-                                                h4("Data Report"),
-                                                fluidRow(
-                                                  column(width = 4, actionButton("report_maid","Generate Data Maid Report", class="btn-success")),
-                                                  column(width = 4, actionButton("report_explorer","Generate Data Explorer", class="btn-primary", style="color:white;")),
-                                                  column(width = 4, actionButton("report_smart","Generate Data Smart", class="btn-warning", style="color:white;"))
-                                                ),
-                                                #actionButton("report_maid","Generate Data Maid Report", class="btn-success"),
-                                                #actionButton("report_explorer","Generate Data Explorer", class="btn-primary", style="color:white;margin-right:5px;"),
-                                                #actionButton("report_smart","Generate Data Smart", class="btn-warning", style="color:white;margin-right:5px;"),
-                                                verbatimTextOutput("error"),
-                                                htmlOutput("report")
-                                
-                                                ),
-                                       tabPanel("Data Viz",
-                                                h2("Univariate"),
-                                                h3("Scatter Plot"),
-                                                h5("X and Y are selected from dropdown menus below."),
-                                                fluidRow(
-                                            
-                                                column(width = 3,selectInput("column1_tabplot", "Select X axis", NULL)),
-                                                column(width = 3,selectInput("column2_tabplot", "Select Y axis", NULL)),
-                                                column(width = 3,selectInput("column3_tabplot", "Size", NULL))),# end fluidRow
-                                                plotlyOutput("plot", height=600)),
+                                       
                                        tabPanel("CDF Composite",
                                                 h3("Cumulative Density Function"),
                                                 fluidRow(
@@ -74,7 +134,9 @@ ui <- dashboardPage(skin="black",
                                                 h5("Plot shows different Cumulative Density Function (CDF) curves."),
                                                 h5("The empirical CDF showed in black dotted-line indicating the actual data, in comparision with other theoretical CDFs which computed from normal and lognormal parameters.",style="color:black;"),
                                                 h5("The plot can be used to quickly interpret which distribution type could be matched the data.",style="color:green;"),
-                                                plotOutput("plot1",width=800, height=600)),
+                                                plotOutput("plot1",width=800, height=600),
+                                                verbatimTextOutput("error2")),
+                                       
                                        tabPanel("Goodness of Fit",
                                                 h4("Goodness of Fit Test", style="color:green;"),
                                                 h5("Data is selected from the tab CDF Comp > Univariate Paramter"),
@@ -85,7 +147,9 @@ ui <- dashboardPage(skin="black",
                                                   ),
                                                 h4("Visual Check:",style="color:red;"),
                                                 h5("From left-right and top-bottom are histogram, quantile-quantile (Q-Q), cumulative density function, and probability-probability plots."),
-                                                plotOutput("plot2", height=800),
+                                                plotOutput("plot2", height=600),
+                                                verbatimTextOutput("error3"),
+                                                
                                                 h4("Statistical Test:",style="color:red;"),
                                                 selectInput("column2_tabgof", "Statistical Method",c("Chi-square"="chisq","Shapiro-Wilk"="sw","Kolmogorov-Smirnov"="ks")),
                                                 h5("Goodness-of-Fit Test is interpreted using Null Hypothesis."),
@@ -100,37 +164,17 @@ ui <- dashboardPage(skin="black",
                                                 h5("Uniform Distribution"),
                                                 h5("Gamma Distribution"),
                                                 h5("Null Hypothesis"),
+                                                h4("Statistical Test"),
+                                                h5("Chi-square"),
+                                                h5("Shapiro-Wilk"),
+                                                h5("Kolmogorov-Smirnov"),
                                                 uiOutput("markdown"))
  
                                        ) # end tabBox
+                                       )
                                 )
                       )
                     )
                     ) # end dashboardPage
 
-# fluidPage(
-#   title = 'Goodness of Fit Statistical Test',
-#   h1('Goodness of Fitness'),
-#   titlePanel("Upload CSV file and Table/Plot Display"),
-#   
-#   sidebarLayout(
-#     sidebarPanel(
-#       fileInput("file", "Choose CSV File", accept = ".csv")
-#     ),
-#       checkboxInput("header","Header", TRUE)),
-#       selectInput("column1", "Select X Column", NULL),
-#       selectInput("column2", "Select Y Column", NULL),
-#       
-#       selectInput("column3", "Select data for GoF", NULL),
-#       selectInput("column4", "Distribution:",c("Normal"="norm","LogNormal"="lnorm","Gamma"="gamma","Exponential"="exp")),
-#       selectInput("column5", "Statistical Test Method:",c("Chi-squared"="chisq")),
-#     mainPanel(
-#       tabsetPanel(
-#         tabPanel("Data Table", DT::dataTableOutput("table")),
-#         tabPanel("Data Plot", plotOutput("plot")),
-#         tabPanel("CDF Comp", plotOutput("plot1")),
-#         tabPanel("Goodness of Fitness", plotOutput("plot2"), verbatimTextOutput("fitdist")),
-#         tabPanel("Markdown")
-#         )
-#     )
-#   )
+
